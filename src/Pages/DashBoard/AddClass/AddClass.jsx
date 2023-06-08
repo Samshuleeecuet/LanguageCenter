ff7e646e15a15f2a8723246e67e752a060bfa2f0
Samshuleeecuet <samshuleeeweb@gmail.com>
2023-06-09 01:49:41 +0600

    add new class page create and designed

--- src/Pages/DashBoard/AddClass/AddClass.jsx
+++ src/Pages/DashBoard/AddClass/AddClass.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { useForm } from "react-hook-form";
+import { AuthContext } from '../../../Provider/AuthProvider';
 const AddClass = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const {user} = useContext(AuthContext);
+    const onSubmit = data => {
+        data.status = 'pending'
+        console.log(data)
+    }
     return (
-        <div>
-            <p>Add Class</p>
+        <div className='min-h-screen'>
+             <h2 className='text-center text-2xl font-extrabold pb-10 '> Add New Class</h2>
+            <form className='border-2 p-5 rounded-lg w-full' onSubmit={handleSubmit(onSubmit)}>
+               
+               <input className='border rounded-lg h-12 w-96 pl-4 mb-4 border-green-400' defaultValue="" type='text' placeholder='Enter Class Name' {...register("classname",{required: true})} required/>
+               <br/>
+               <input className='border rounded-lg h-12 w-96 pl-4 mb-4 border-green-400' defaultValue="" type='text' placeholder='Enter Class Image' {...register("classimage",{required: true})} required/>
+               <br/>
+               <input className='border rounded-lg h-12 w-96 pl-4 mb-4 border-green-400' value={user?.displayName} type='text' placeholder='Enter Instructor Name' {...register("instructorname")} required/>
+               <br/>
+               <input className='border rounded-lg h-12 w-96 pl-4 mb-4 border-green-400' value={user?.email} type='text' placeholder='Enter Instructor Email' {...register("instructoremail")} required/>
+               <br/>
+               <input className='border rounded-lg h-12 w-44 pl-4 mr-4 mb-4 border-green-400' defaultValue="" type='text' placeholder='Enter Available Seat' {...register("availableseat",{required: true})} required/>
+               <input className='border rounded-lg h-12 w-44 pl-4 mb-4 border-green-400' defaultValue="" type='text' placeholder='Enter Price' {...register("price",{required: true})} required/>
+               <br/>
+              
+               <input className='btn btn-accent text-white' type="submit" value="Add Class" />
+            </form>
         </div>
     );
 };
